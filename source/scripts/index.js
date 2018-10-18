21020cc8c7516b9295b7b83efcbb25e494896788
Dobi Okrasa <dobi.okrasa@gmail.com>
2018-10-18 16:36:02 +0200

    Refactor slider function

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,33 +3,36 @@ import 'jquery-ui/ui/widgets/slider';
 
 window.onload = function() {
 
-  function handleSlider($slider, min, max, currentValues, step) {
+  function formatPrice(price) {
+    let string = price.toString();
+    return price = string.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  }
 
-    let children = $slider.find('[data-name]'),
-        inputMax,
-        inputMin;
+  $.each( $('.slider'), function( index, value ) {
 
-    $.each(children, function( index, value ) {
-      $(value).data('name') == "min" ? inputMin = value : inputMax = value;
-    });
+    let inputMax = $(this).find('[data-name="max"]'),
+        inputMin = $(this).find('[data-name="min"]');
 
-    return ( $slider.slider({
+    const data = $(this).find('[data-name]'),
+          min = inputMin.data('min'),
+          max = inputMax.data('max'),
+          currentMin = inputMin.data('current-min'),
+          currentMax = inputMax.data('current-max'),
+          step = data.data('step'),
+          currentArray = [currentMin, currentMax];
+
+    $(this).slider({
       range: true,
       min: min,
       max: max,
-      values: currentValues,
-      step:  step,
+      values: currentArray,
+      step: step,
       slide: function( event, ui ) {
-        $(inputMin).val( ui.values[ 0 ]);
-        $(inputMax).val( ui.values[ 1 ] );
+        $(inputMin).val( formatPrice(ui.values[ 0 ]) );
+        $(inputMax).val( formatPrice(ui.values[ 1 ]) );
       }
     })
-    );
-  }
-
-  handleSlider($('#slider-area'), 25, 120, [35, 75], 5);
-  handleSlider($('#slider-price'), 190000, 350000, [ 190000, 250000 ], 20000);
-  handleSlider($('#slider-rooms'), 1, 4, [1, 3], 1);
+  });
 
 }
 
